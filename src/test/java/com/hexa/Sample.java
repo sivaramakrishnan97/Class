b8e2c04f34f4f2f0edd447d4b2eff0afb8d457ff
sivaramakrishnan97 <sivaraman.drs@gmail.com>
2023-04-06 06:57:58 +0530

    message

---
+++ src/test/java/com/hexa/Sample.java
@@ -0,0 +1,17 @@
+package com.hexa;
+
+public class Sample {
+	
+	public void test11() {
+		System.out.println("test 11");
+	} 
+	
+	public void test12() {
+		System.out.println("test 12");
+	} 
+	
+	public void test13() {
+		System.out.println("test 13");
+	} 
+
+}
