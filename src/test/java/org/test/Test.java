24a9c2c4bafbc31ac8cdcdea58df88b64ce32d55
sivaramakrishnan97 <sivaraman.drs@gmail.com>
2023-04-13 15:31:01 +0530

    Git class 1

---
+++ src/test/java/org/test/Test.java
@@ -0,0 +1,17 @@
+package org.test;
+
+public class Test {
+
+	private void Tamil() {
+		System.out.println("tamil");
+	}
+
+	private void English() {
+		System.out.println("english");
+	}
+
+	private void Telugu() {
+		System.out.println("telugu");
+	}
+
+}
